c45436c802bed2d879cc620569f51ffe4b3a7a9a
EduardoAlvesNeto <edualvezneto@gmail.com>
2024-11-06 20:46:11 -0300

    feat: add styled-components

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,26 @@
+'use client';
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        -webkit-font-smoothing: antialiased;
+        box-sizing: border-box;
+        outline: 0;
+        border: 0;
+        padding: 0;
+        margin: 0;
+    }
+
+    :root {
+        --black: #171717;
+        --white: #ECECEC;
+        --gray: #3A3A3A;
+        --primary: #3DB78C;
+        --secondary: #13131E;
+    }
+
+    html, body {
+        background-color: var(--black);
+    }
+`
